8f2aa3a405fe6542257cae3a4de5645eb2084e96
Andro <andrija.slovic1@gmail.com>
2024-07-07 01:02:21 +0200

    added film functionalities

---
+++ cloud-back/downloadFile/download_file.py
@@ -0,0 +1,22 @@
+import base64
+import boto3
+import json
+
+from utility.utils import create_response
+
+s3 = boto3.client("s3")
+
+def download(event, context):
+
+    filename = event['queryStringParameters']['filename']
+    response = s3.get_object(Bucket='film-bucket', Key=filename)
+    file_data = base64.b64encode(response["Body"].read()).decode()
+    return {
+        'statusCode': 200,
+        'body': file_data,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': response['ContentType'],
+            'Content-Disposition': f'attachment; filename={filename}'
+        }
+    }
---
+++ cloud-back/getFilms/get_films.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+
+from utility.utils import create_response
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'films-table'
+
+def get_all(event, context):
+    table = dynamodb.Table(table_name)
+    response = table.scan()
+    body = {
+        'data': response['Items']
+    }
+    print(body)
+    return create_response(200, body)
