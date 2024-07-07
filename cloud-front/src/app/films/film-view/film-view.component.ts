8685deeb59477825736b2dacfe0c065660ef5247
MVojkan <mvojkan02.7@gmail.com>
2024-07-07 04:04:05 +0200

    added detailed information for films

--- cloud-front/src/app/films/film-view/film-view.component.ts
+++ cloud-front/src/app/films/film-view/film-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-film-view',
   standalone: true,
@@ -7,8 +7,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './film-view.component.html',
   styleUrl: './film-view.component.css'
 })
+
 export class FilmViewComponent {
   @Input() film: any = null;
   @Output() download = new EventEmitter();
+
+  constructor(public datePipe: DatePipe){};
 
 }
