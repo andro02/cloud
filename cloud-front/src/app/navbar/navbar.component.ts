ea0932f61b764d97ab924dbdf7bc036f48ff1bb3
Andro <andrija.slovic1@gmail.com>
2024-07-08 19:45:23 +0200

    added logout in navbar

--- cloud-front/src/app/navbar/navbar.component.ts
+++ cloud-front/src/app/navbar/navbar.component.ts
@@ -1,14 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AxiosService } from '../axios.service';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(public axiosService: AxiosService, private router: Router) {}
+
+  logOut(): void {
+    this.axiosService.setAuthToken(null);
+    this.router.navigate(['auth/login']);
+  }
 
 }
