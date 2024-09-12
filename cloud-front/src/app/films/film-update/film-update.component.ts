313427b00422a3f08b42682b86fb58b45f3cc868
Andro <andrija.slovic1@gmail.com>
2024-09-12 05:19:39 +0200

    load file on update page

--- cloud-front/src/app/films/film-update/film-update.component.ts
+++ cloud-front/src/app/films/film-update/film-update.component.ts
@@ -1,18 +1,19 @@
-import { DatePipe } from '@angular/common';
-import { Component, inject, Input } from '@angular/core';
-import {  FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, ElementRef, inject, Input, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AxiosService } from '../../axios.service';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Buffer } from 'buffer'; 
 
 @Component({
   selector: 'app-film-update',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './film-update.component.html',
   styleUrl: './film-update.component.css'
 })
 export class FilmUpdateComponent {
-  // @Input() film: any = null;
+  @ViewChild('fileInput') fileInput!: ElementRef;
   route: ActivatedRoute = inject(ActivatedRoute);
   filename = '';
   film: any;
@@ -24,14 +25,54 @@ export class FilmUpdateComponent {
 
     this.filmForm = this.fb.group({
       file: [null, Validators.compose([Validators.required])],
-      fileToUpload: new FormControl(null),
       name: [null, Validators.compose([Validators.required])],
+      fileToUpload: new FormControl(null),
       description: [null, Validators.compose([Validators.required])],
       director: [null, Validators.compose([Validators.required])],
       genre: [null, Validators.compose([Validators.required])],
       actors: [null, Validators.compose([Validators.required])],
       releaseDate: [null, Validators.compose([Validators.required])],
     })
+  }
+
+  fileError: boolean = false;
+  errorMessage: string = '';
+
+  onFileChanged(event: Event) {
+    if ((event.target as HTMLInputElement).files?.length == 0) {
+      var fileLabel = document.getElementsByClassName('custom-file-label')[0] as HTMLFormElement;
+      fileLabel.classList.remove("selected");
+      fileLabel.innerHTML = 'Choose file';
+      return;
+    }
+
+    const file = (event.target as HTMLInputElement).files![0];
+    this.filmForm.patchValue({ fileToUpload: file });
+
+    const validTypes = [
+      'video/mp4', 
+      'video/x-matroska', // For .mkv
+      'video/ogg',
+      'video/webm',
+      'video/quicktime',
+      'binary/octet-stream'
+    ]
+
+    if(!validTypes.includes(file.type)){
+      var fileLabel = document.getElementsByClassName('custom-file-label')[0] as HTMLFormElement;
+      fileLabel.classList.remove("selected");
+      fileLabel.innerHTML = 'Choose file';
+      this.fileError = true;
+      this.errorMessage = 'Invalid file type! Please select a valid video file (.mp4, .mkv, .mov, .webm, .ogg)';
+      return;
+    }
+
+    var fileLabel = document.getElementsByClassName('custom-file-label')[0] as HTMLFormElement;
+    fileLabel.classList.add("selected");
+    fileLabel.innerHTML = file['name'];
+    this.fileError = false;
+    this.errorMessage = '';
+
   }
 
   ngOnInit(): void {
@@ -54,6 +95,27 @@ export class FilmUpdateComponent {
           actors: this.film.actors,
           releaseDate: this.film.releaseDate,
         });
+
+        const query = "?filename=" + this.filename;
+        this.axiosService.request(
+          "GET",
+          "/downloadFilm" + query,
+          null,
+          "application/json"
+        ).then(
+          response => {
+            const contentType = response["headers"]["content-type"];
+            const file = Buffer.from(response.data, "base64");
+            const blob = new Blob([file], { type: contentType });
+            const fileUpload = new File([blob], this.filename, { type: contentType });
+            const dataTransfer = new DataTransfer();
+            dataTransfer.items.add(fileUpload);
+            this.fileInput.nativeElement.files = dataTransfer.files;
+
+            const event = new Event('change', { bubbles: true });
+            this.fileInput.nativeElement.dispatchEvent(event);
+          }
+        );
       }
     );
     
@@ -63,18 +125,12 @@ export class FilmUpdateComponent {
     var form = document.getElementsByClassName('needs-validation')[0] as HTMLFormElement;
 
     if (form.checkValidity() !== false) {
+
       const uploadFileData = { ...this.filmForm.value };
       const fileToUpload = this.filmForm.controls["fileToUpload"].value;
       const filename = fileToUpload['name'];
 
       const query = "?filename=" + filename;
-
-      // this.axiosService.request(
-      //   "PUT",
-      //   "/uploadFilm" + query,
-      //   fileToUpload,
-      //   "multipart/form-data"
-      // );
 
       const fileInformation = {
         'filename': filename,
@@ -91,13 +147,29 @@ export class FilmUpdateComponent {
         'userEmail': this.axiosService.getEmail()
       }
 
+      //TODO PROMENI URLS
       // this.axiosService.request(
-      //   "PUT",
-      //   "/film",
-      //   fileInformation,
-      //   "application/json"
+      //   "POST",
+      //   "/uploadFilm" + query,
+      //   fileToUpload,
+      //   "multipart/form-data"
+      // ).then(
+      //   response => {
+      //     this.axiosService.request(
+      //     "POST",
+      //     "/film",
+      //     fileInformation,
+      //     "application/json"
+      //     ).then(
+      //       response => {
+      //         this.router.navigate(['films']);
+      //       }
+      //     );
+      //   }
       // );
     }
+    
+    form.classList.add('was-validated');
   }
   
 
